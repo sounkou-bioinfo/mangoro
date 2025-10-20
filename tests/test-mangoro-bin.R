c5a17acf5f06f385528e07a0f617171e4453d943
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-10-21 01:02:11 +0400

    sleep a bit, why ?

--- tests/test-mangoro-bin.R
+++ tests/test-mangoro-bin.R
@@ -5,30 +5,33 @@ library(processx)
 # vendored mangos version
 get_mangos_version()
 go_echo_code <- paste(
-  "package main",
-  "import (",
-  '  "os"',
-  '  "go.nanomsg.org/mangos/v3/protocol/rep"',
-  '  _ "go.nanomsg.org/mangos/v3/transport/ipc"',
-  ")",
-  "func main() {",
-  "  url := os.Args[1]",
-  "  sock, _ := rep.NewSocket()",
-  "  sock.Listen(url)",
-  "  for {",
-  "    msg, _ := sock.Recv()",
-  '    newMsg := append(msg, []byte(" [echoed by Go]")...)',
-  "    sock.Send(newMsg)",
-  "  }",
-  "}",
-  sep = "\n"
+    "package main",
+    "import (",
+    '  "os"',
+    '  "go.nanomsg.org/mangos/v3/protocol/rep"',
+    '  _ "go.nanomsg.org/mangos/v3/transport/ipc"',
+    ")",
+    "func main() {",
+    "  url := os.Args[1]",
+    "  sock, _ := rep.NewSocket()",
+    "  sock.Listen(url)",
+    "  for {",
+    "    msg, _ := sock.Recv()",
+    '    newMsg := append(msg, []byte(" [echoed by Go]")...)',
+    "    sock.Send(newMsg)",
+    "  }",
+    "}",
+    sep = "\n"
 )
 
 tmp_go <- tempfile(fileext = ".go")
 writeLines(go_echo_code, tmp_go)
 
-tmp_bin <- if (.Platform$OS.type == "windows") tempfile(fileext = ".exe") else
-  tempfile()
+tmp_bin <- if (.Platform$OS.type == "windows") {
+    tempfile(fileext = ".exe")
+} else {
+    tempfile()
+}
 mangoro_go_build(tmp_go, tmp_bin)
 
 ipc_url <- create_ipc_path()
@@ -39,15 +42,22 @@ Sys.sleep(3)
 sock <- nanonext::socket("req", dial = ipc_url)
 msg <- charToRaw("hello from R")
 send_result <- nanonext::send(sock, msg, mode = "raw")
-if (nanonext::is_error_value(send_result)) {
-  Sys.sleep(1)
-  send_result <- nanonext::send(sock, msg, mode = "raw")
+# Retry send up to 5 times if error
+max_attempts <- 5
+attempt <- 1
+while (nanonext::is_error_value(send_result) && attempt < max_attempts) {
+    Sys.sleep(1)
+    send_result <- nanonext::send(sock, msg, mode = "raw")
+    attempt <- attempt + 1
 }
 print(send_result)
+# Retry recv up to 5 times if error
 rep <- nanonext::recv(sock, mode = "raw")
-if (nanonext::is_error_value(rep)) {
-  Sys.sleep(1)
-  rep <- nanonext::recv(sock, mode = "raw")
+attempt <- 1
+while (nanonext::is_error_value(rep) && attempt < max_attempts) {
+    Sys.sleep(1)
+    rep <- nanonext::recv(sock, mode = "raw")
+    attempt <- attempt + 1
 }
 Sys.sleep(3)
 print(rawToChar(rep))
