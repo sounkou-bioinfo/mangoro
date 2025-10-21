1d7d6c1de890343bf225b4b2c0c9ab78e8b56c6e
sounkou-bionfo <sounkoutoure@gmail.com>
2025-10-21 17:49:55 +0400

    macos crap

--- R/mangoro-bin.R
+++ R/mangoro-bin.R
@@ -32,22 +32,6 @@ run_mangoro_bin <- function(name, args = character(), ...) {
   processx::process$new(bin, args = args, ...)
 }
 
-#' Generate a platform-correct IPC URL for mangoro
-#'
-#' @param prefix Prefix for the temp file (default: "mangoro-echo")
-#' @return IPC URL string suitable for nanonext and mangoro Go binaries
-#' @export
-mangoro_ipc_url <- function(prefix = "mangoro-echo") {
-  tmp_ipc <- tempfile(pattern = prefix, fileext = ".sock")
-  if (.Platform$OS.type == "windows") {
-    ipc_url <- paste0("ipc://", gsub("/", "\\\\", tmp_ipc))
-  } else {
-    tmp_ipc <- gsub("/+", "/", x = tmp_ipc)
-    ipc_url <- paste0("ipc://", tmp_ipc)
-  }
-  ipc_url
-}
-
 #' Create a unique IPC path for mangoro
 #'
 #' @param prefix Prefix for the temp file (default: "mangoro-echo")
